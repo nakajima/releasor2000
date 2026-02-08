221333a9c42ec20de7e7687a9bf0fff259609907
Pat Nakajima <patnakajima@gmail.com>
2026-02-08 11:30:45 -0800

    Update releasor2000 to 0.0.5

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   outputs = { self, nixpkgs }: let
     mkPkg = { target, sha256 }: let
-      pkgs = nixpkgs.legacyPackages.${"$"}{system};
+      pkgs = nixpkgs.legacyPackages.${system};
     in pkgs.stdenv.mkDerivation {
       pname = "releasor2000";
       version = "0.0.5";
@@ -19,19 +19,19 @@
     systems = {
           "x86_64-darwin" = mkPkg {
             target = "x86_64-apple-darwin";
-            sha256 = "68826491c132c87392c407082f9064d79ffc1f5462a68935f0392a89f125c3b9";
+            sha256 = "ba9483eae30d2db136d04a4a3a76fbe6da66da108dade8af992fb831baf0dba9";
           };
           "aarch64-darwin" = mkPkg {
             target = "aarch64-apple-darwin";
-            sha256 = "552141cb7e6a4e1568b0d49118effebee6a7a5b102e9b74d268b4f688c78e249";
+            sha256 = "3ea818ef7356283f53604e745d5121f109387fcab10a0dd68590c22a44ba03fa";
           };
           "x86_64-linux" = mkPkg {
             target = "x86_64-unknown-linux-gnu";
-            sha256 = "1a9f668d89acfe8f7a8fb86c1d68e673c9fbe37fbe063fd9f4ce1c8dcada3e58";
+            sha256 = "51a45acfba56034cbf8e7e449b128acfa2c0f9dd0831939c05676bffc5e0e056";
           };
           "aarch64-linux" = mkPkg {
             target = "aarch64-unknown-linux-gnu";
-            sha256 = "7c333db2f2054088654c111c2e604e7823220c9c4922da3130c5ab7be99998a4";
+            sha256 = "1da5a5a588bf4ab4a75d5fff3b32c23e5b78944084643c5f0d67376c10f92488";
           };
     };
   in {
