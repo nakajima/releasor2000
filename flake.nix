7adc78ec70e35c5ff8096d9a47675837ef89e9b6
Pat Nakajima <patnakajima@gmail.com>
2026-02-08 12:19:59 -0800

    Update releasor2000 to 0.0.5

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
           version = "0.0.5";
           src = pkgs.fetchurl {
             url = "https://github.com/nakajima/releasor2000/releases/download/v0.0.5/releasor2000-0.0.5-x86_64-apple-darwin.tar.gz";
-            sha256 = "ef40ddf8dbd917139e5265ed6894eaa083b0740b67f59f2d300e0488670b6b22";
+            sha256 = "f249ea8a6b4da98d622ba208f9f56a8958342b15c5a86dd148e1dbf3488767f4";
           };
           sourceRoot = ".";
           installPhase = ''
@@ -27,7 +27,7 @@
           version = "0.0.5";
           src = pkgs.fetchurl {
             url = "https://github.com/nakajima/releasor2000/releases/download/v0.0.5/releasor2000-0.0.5-aarch64-apple-darwin.tar.gz";
-            sha256 = "e544258f2b2e27f6e0f6e315d3bb5de490acbc8f202f4f86c5e9862efaee867c";
+            sha256 = "95f878e6600147b188687a4edd9ad3e75d8c84e80931c9a428a9883c4c94111e";
           };
           sourceRoot = ".";
           installPhase = ''
@@ -42,7 +42,7 @@
           version = "0.0.5";
           src = pkgs.fetchurl {
             url = "https://github.com/nakajima/releasor2000/releases/download/v0.0.5/releasor2000-0.0.5-x86_64-unknown-linux-gnu.tar.gz";
-            sha256 = "c2ab15bb5160c7782817ca87e2e067773d08acd6a0896051791d4be78244841b";
+            sha256 = "0e8520c320b7a027e50129c25c5afb7819b2f459a74cfeeb80232cbab06e0ca2";
           };
           sourceRoot = ".";
           installPhase = ''
@@ -57,7 +57,7 @@
           version = "0.0.5";
           src = pkgs.fetchurl {
             url = "https://github.com/nakajima/releasor2000/releases/download/v0.0.5/releasor2000-0.0.5-aarch64-unknown-linux-gnu.tar.gz";
-            sha256 = "9a7eb526a9116098ab300aacf303ba5a343cc999f909252cb25a5df93e183722";
+            sha256 = "56b8e109f04aaf1b50288a3d0b5bb9b67f5795ee8a1c98cd28d540878d48b202";
           };
           sourceRoot = ".";
           installPhase = ''
